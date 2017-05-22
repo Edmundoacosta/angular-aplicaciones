5de30f2184d86cad928b2015ea98573037947e0e
Alumno10 S304 <a10s304@campustls.local>
2017-05-22 08:52:29 -0500

    avanzo

--- src/app/lista/formulario/formulario.component.ts
+++ src/app/lista/formulario/formulario.component.ts
@@ -1,13 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Producto } from '../producto';
 
 @Component({
   selector: 'app-formulario',
   templateUrl: './formulario.component.html',
-  styleUrls: ['./formulario.component.css']
 })
 export class FormularioComponent implements OnInit {
 
+	@Output('onProductoAdd') productoAgregado = new EventEmitter<Producto>();
+
   constructor() { }
+
+  agregarProducto(producto: HTMLInputElement) {
+  	this.productoAgregado.emit({producto: producto.value});
+  	producto.value = '';
+  	producto.focus();
+  }
 
   ngOnInit() {
   }
